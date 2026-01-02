3c6c1be4f25c427347ef0a167334bc364d6d9192
SanjayvVarma <spverma.sanjay56056@gmail.com>
2026-01-02 09:14:55 +0530

    add InputBox component with styling

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, TextInput, View } from 'react-native';
+
+const InputBox = ({ placeholder }) => {
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.textInput}
+                placeholder={placeholder}
+            />
+        </View>
+    )
+}
+
+export default InputBox;
+
+const styles = StyleSheet.create({
+    textInput: {
+        borderWidth: 1,
+        width: 340,
+        borderColor: 'gray',
+        borderRadius: 5,
+        fontSize: 15,
+        padding: 10,
+        backgroundColor: "#ccc5c5ff"
+    }
+})
