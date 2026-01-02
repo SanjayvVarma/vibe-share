e81dac7ebd545a443ee3202f8e7588611f6c87ff
SanjayvVarma <spverma.sanjay56056@gmail.com>
2026-01-02 09:15:02 +0530

    add CustomBtn component with styles

---
+++ src/components/CustomBtn.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+const CustomBtn = ({ btnTitle, }) => {
+    return (
+        <TouchableOpacity>
+            <View style={styles.btn}>
+                <Text style={styles.btnTxt}>{btnTitle}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+export default CustomBtn;
+
+const styles = StyleSheet.create({
+    btn: {
+        width: 340,
+        borderRadius: 5,
+        backgroundColor: '#0779f1ff',
+    },
+
+    btnTxt: {
+        fontSize: 18,
+        color: "#fff",
+        paddingVertical: 9,
+        fontSize: 18,
+        textAlign: "center"
+    }
+})
